1e19f95a5d5cf00b69911af01b780b36e338a259
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2018-07-15 21:35:53 +0200

    Dodanie własnej kontrolki

---
+++ Sulmar.WPFMVVM.Advanced.WPFClient/Views/CustomControlsView.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Sulmar.WPFMVVM.Advanced.WPFClient.Views
+{
+    /// <summary>
+    /// Interaction logic for CustomControlsView.xaml
+    /// </summary>
+    public partial class CustomControlsView : Window
+    {
+        public CustomControlsView()
+        {
+            InitializeComponent();
+        }
+
+        private void MyCustomControl_Tap(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("It is the custom routed event of your custom control");
+        }
+    }
+}
