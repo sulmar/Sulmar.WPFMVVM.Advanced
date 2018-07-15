a776ef1a0b87c229dda7c5404831cd6adacdcbcd
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2018-07-15 16:18:22 +0200

    Wirtualizacja

---
+++ Sulmar.WPFMVVM.Advanced.WPFClient/ViewModels/BaseViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace Sulmar.WPFMVVM.Advanced.WPFClient.ViewModels
+{
+    public abstract class BaseViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string propname = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propname));
+        }
+    }
+}
--- Sulmar.WPFMVVM.Advanced.WPFClient/ViewModels/MarkupExtensionsViewModel.cs
+++ Sulmar.WPFMVVM.Advanced.WPFClient/ViewModels/MarkupExtensionsViewModel.cs
@@ -9,19 +9,35 @@ using System.Windows.Input;
 
 namespace Sulmar.WPFMVVM.Advanced.WPFClient.ViewModels
 {
-    public class MarkupExtensionsViewModel
+    public class MarkupExtensionsViewModel : BaseViewModel
     {
+        private IEnumerable<string> _items;
+
         public Status SelectedStatus { get; set; } = Status.Completed;
+
+
+        public IEnumerable<string> Items
+        {
+            get => _items;
+            set
+            {
+                _items = value;
+
+                OnPropertyChanged();
+            }
+        }
 
         public MarkupExtensionsViewModel()
         {
             LoadCommand = new RelayCommand(p => Load());
             SendCommand = new RelayCommand(p => Send(), p => CanSend);
+
+
         }
 
         private void Load()
         {
-            
+            Items = Enumerable.Range(1, 10000).Select(x => $"Item {x}");
         }
 
         private void Send()
