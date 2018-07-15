b974b7a06b57d7a2d3febc1a5e538b8565d076cc
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2018-07-15 09:44:46 +0200

    Init

---
+++ Sulmar.WPFMVVM.Advanced.WPFClient/MarkupExtensions/SplitBinding.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Markup;
+using System.Xml.Linq;
+
+namespace Sulmar.WPFMVVM.Advanced.WPFClient.MarkupExtensions
+{
+    public class SplitBinding : MarkupExtension
+    {
+        public string Source { get; set; }
+        public char Separator { get; set; }
+
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            if ((Source != null) && (Separator != null))
+                return Parse(Source, Separator);
+
+            else throw new InvalidOperationException("Inputs cannot be blank");
+        }
+
+        private IList<string> Parse(string source, char separator)
+        {
+            var items = source.Split(separator);
+
+            return items;
+
+        }
+    }
+}
