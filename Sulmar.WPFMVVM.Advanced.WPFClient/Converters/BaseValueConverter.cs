a72200ba7109c53386a64360be48f1751014d276
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2018-07-15 15:13:41 +0200

    Dodanie konwertera

---
+++ Sulmar.WPFMVVM.Advanced.WPFClient/Converters/BaseValueConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Markup;
+
+namespace Sulmar.WPFMVVM.Advanced.WPFClient.Converters
+{
+    public abstract class BaseValueConverter<T> : MarkupExtension, IValueConverter
+        where T : class, new()
+    {
+        private static T converter = null;
+
+        public abstract object Convert(object value, Type targetType, object parameter, CultureInfo culture);
+
+        public abstract object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture);
+
+
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return converter ?? (converter = new T());
+        }
+    }
+}
---
+++ Sulmar.WPFMVVM.Advanced.WPFClient/Converters/StatusConverter.cs
@@ -0,0 +1,34 @@
+﻿using Sulmar.WPFMVVM.Advanced.WPFClient.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace Sulmar.WPFMVVM.Advanced.WPFClient.Converters
+{
+    public class StatusConverter : BaseValueConverter<StatusConverter>
+    {
+        public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            switch((Status)value)
+            {
+                case Status.Created: return Brushes.Blue;
+                case Status.InProgress: return Brushes.Red;
+                case Status.Completed: return Brushes.Green;
+
+                default:
+                    Debugger.Break();
+                    throw new NotSupportedException();
+            }
+        }
+
+        public override object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
