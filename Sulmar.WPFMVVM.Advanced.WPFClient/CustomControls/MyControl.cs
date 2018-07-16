7295c3c17e5aee8ef55e74fbfe2b5f10757ad48b
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2018-07-16 21:44:42 +0200

    ContentProperty

---
+++ Sulmar.WPFMVVM.Advanced.WPFClient/CustomControls/MyControl.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Markup;
+
+namespace Sulmar.WPFMVVM.Advanced.WPFClient.CustomControls
+{
+    [ContentProperty("MainContent")]
+    public class MyControl : Control
+    {
+        public MyControl()
+        {
+            DefaultStyleKey = typeof(MyControl);
+        }
+
+        public object MainContent
+        {
+            get { return GetValue(MainContentProperty); }
+            set { SetValue(MainContentProperty, value); }
+        }
+
+        public static readonly DependencyProperty MainContentProperty =
+            DependencyProperty.Register("MainContent", typeof(object), typeof(MyControl), null);
+
+
+        
+    }
+}
