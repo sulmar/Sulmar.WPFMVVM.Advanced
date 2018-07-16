46b657e7163e9ca20113b6156be78dcd2ed10afe
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2018-07-16 23:06:18 +0200

    IsDeferredScrollingEnabled

---
+++ Sulmar.WPFMVVM.Advanced.WPFClient/Views/TransformsView.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Sulmar.WPFMVVM.Advanced.WPFClient.Views
+{
+    /// <summary>
+    /// Interaction logic for TransformsView.xaml
+    /// </summary>
+    public partial class TransformsView : Window
+    {
+        public TransformsView()
+        {
+            InitializeComponent();
+        }
+    }
+}
